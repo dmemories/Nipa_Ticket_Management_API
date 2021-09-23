6e8449b08fcbd87a42a231e56a2b546ae88dc23b
dmemories <spyhack13@yahoo.com>
2021-09-23 21:18:33 +0700

    Complete Update

--- src/controllers/ticket.contoller.ts
+++ src/controllers/ticket.contoller.ts
@@ -12,18 +12,31 @@ export default class ticketController extends Controller {
 
     public initialRoutes() {
         this.router.post('/',  this.createTicket);
+        this.router.put('/',  this.updateTicket);
         this.router.get('/',  this.getAllTicket);
     }
 
     private async createTicket(req: Request, res: Response): Promise<void> {
         try {
             let validReq: object = validation.newTicket(req.body);
-            let newTicket = await (new ticketModel(validReq)).save();
+            let newTicket: object = await (new ticketModel(validReq)).save();
             res.send(newTicket);
         } catch (err) {
-            res.status(400).json({ error: err })
+            res.status(400).json({ error: err });
         }
     }
+
+    private async updateTicket(req: Request, res: Response): Promise<void> {
+        try {
+            let validReq: any = validation.updateTicket(req.body);
+            await ticketModel.findOneAndUpdate({ _id: validReq.id }, validReq);
+            res.json(validReq);
+        } catch (err) {
+            res.status(400).json({ error: err });
+        }
+    }
+     /* await Item.findOneAndUpdate({ _id: req.params.id }, reqBody);
+        res.status(200).json({"result" : `Update Successfully \n${JSON.stringify(reqBody)}\n`}) */
 
     private getAllTicket(req: Request, res: Response): void {
         res.send('Hello World !');
--- src/validation.ts
+++ src/validation.ts
@@ -2,10 +2,24 @@ import joi from "joi";
 
 const newTicket = (reqBody: object): object => {
     const joiObj = joi.object({
-        information: joi.string().min(2).max(40).required(),
-        title: joi.string().min(2).max(40).required(),
-        description: joi.string().min(4).max(100).required(),
-        contact_information: joi.string().min(4).max(100).required(),
+        information: joi.string().min(1).max(40).required(),
+        title: joi.string().min(1).max(40).required(),
+        description: joi.string().min(1).max(100).required(),
+        contact_information: joi.string().min(1).max(100).required(),
+    });
+    const { error } = joiObj.validate(reqBody);
+    if (error) throw error.details[0].message;
+    return reqBody;
+}
+
+const updateTicket = (reqBody: object): object => {
+    const joiObj = joi.object({
+        id: joi.string().min(1).max(40).required(),
+        information: joi.string().min(1).max(40),
+        title: joi.string().min(1).max(40),
+        description: joi.string().min(1).max(100),
+        contact_information: joi.string().min(1).max(100),
+        status: joi.string().min(1).max(10).required()
     });
     const { error } = joiObj.validate(reqBody);
     if (error) throw error.details[0].message;
@@ -13,5 +27,6 @@ const newTicket = (reqBody: object): object => {
 }
 
 export {
-    newTicket
+    newTicket,
+    updateTicket
 }
